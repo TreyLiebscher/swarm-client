a9052ed59878e420199dae5e670dee4e07a009fe
treyliebscher <wcliebsc@gmail.com>
2019-06-05 15:00:42 -0400

    added navbar

--- src/App.js
+++ src/App.js
@@ -1,21 +1,12 @@
 import React from 'react';
 import './App.css';
+import NavBar from './components/navbar';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <NavBar />
       </header>
     </div>
   );
