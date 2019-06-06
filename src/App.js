f5ec792de96f387ec7078429b279e2602c90709b
treyliebscher <wcliebsc@gmail.com>
2019-06-06 10:03:51 -0400

    added router + routes to app base

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
 import React from 'react';
 import './App.css';
 import NavBar from './components/navbar';
-// testing
-import SignupForm from './components/forms/signup-form';
+import routes from './routes';
+import { BrowserRouter as Router } from 'react-router-dom';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <NavBar />
-        <SignupForm />
-      </header>
+      <Router>
+        <div>
+          <header className="App-header">
+            <NavBar />
+            {routes}
+          </header>
+        </div>
+      </Router>
     </div>
   );
 }
