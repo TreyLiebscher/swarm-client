288dc8bb210cfa69ba0e1e9e7fb0b0eb9d1e3f18
treyliebscher <wcliebsc@gmail.com>
2019-06-06 10:09:16 -0400

    added routes WIP

---
+++ src/routes.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Route, Switch } from 'react-router';
+
+// Module root components
+import SignupPage from './pages/signup-page';
+import LogInPage from './pages/login-page';
+import NotFoundPage from './pages/notfound-page';
+
+
+
+export default (
+  <Switch>
+    {/* <Route exact path="/" component={HomePage} />
+    <Route path="/search/:searchString" component={HomePage} /> */}
+    <Route exact path="/signup" component={SignupPage} />
+    <Route exact path="/login" component={LogInPage} />
+    <Route path="*" component={NotFoundPage} />
+  </Switch>
+);
