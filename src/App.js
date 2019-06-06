6e4db1b693cbcefeaca467f2f9009fa8e7c400b6
treyliebscher <wcliebsc@gmail.com>
2019-06-06 08:13:35 -0400

    added signup form to home page (temp)

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import './App.css';
 import NavBar from './components/navbar';
+// testing
+import SignupForm from './components/forms/signup-form';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <NavBar />
+        <SignupForm />
       </header>
     </div>
   );
