a8ed36be129653ae710881d0b54909888e8bc02d
treyliebscher <wcliebsc@gmail.com>
2019-06-06 12:20:45 -0400

    added homepage route

--- src/routes.js
+++ src/routes.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Route, Switch } from 'react-router';
 
 // Module root components
+import HomePage from './pages/home-page';
 import SignupPage from './pages/signup-page';
 import LogInPage from './pages/login-page';
 import NotFoundPage from './pages/notfound-page';
@@ -10,7 +11,7 @@ import NotFoundPage from './pages/notfound-page';
 
 export default (
   <Switch>
-    {/* <Route exact path="/" component={HomePage} />
+    <Route exact path="/" component={HomePage} />
     <Route path="/search/:searchString" component={HomePage} /> */}
     <Route exact path="/signup" component={SignupPage} />
     <Route exact path="/login" component={LogInPage} />
