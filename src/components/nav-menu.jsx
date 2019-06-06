c13bb2f32b7bcfa6e7727a072634309c033c57e8
treyliebscher <wcliebsc@gmail.com>
2019-06-06 10:07:14 -0400

    added links to signup + login + logout

--- src/components/nav-menu.jsx
+++ src/components/nav-menu.jsx
@@ -1,8 +1,41 @@
 import React, { useState } from 'react';
+import {connect} from 'react-redux';
+import {Link, Redirect, withRouter} from 'react-router-dom';
+import {clearAuth} from '../actions/auth';
+import {clearAuthToken} from '../local-storage';
 import './nav-menu.css';
 
-export function NavMenu(){
+export function NavMenu(props){
     const [active, setActive] = useState(false);
+
+    function logOut(){
+        props.dispatch(clearAuth());
+        clearAuthToken();
+        // <Redirect to="/" />
+    }
+
+
+
+    const button_manager = () => {
+        if(props.loggedIn){
+            return (
+                <ul className="navmenu-list">
+                    <li className="navmenu-item"><button onClick={() => logOut()}>Log Out</button></li>                    
+                    <li className="navmenu-item">About</li>
+                    <li className="navmenu-item">Your Profile</li>
+                </ul>
+            )
+        } else {
+            return (
+                <ul className="navmenu-list">
+                    <li className="navmenu-item" onClick={() => setActive(false)}><Link to="/login" className="navmenu-link">Login</Link></li>
+                    <li className="navmenu-item" onClick={() => setActive(false)}><Link to="/signup" className="navmenu-link">Signup</Link></li>                    
+                    <li className="navmenu-item">About</li>
+                    <li className="navmenu-item">Your Profile</li>
+                </ul>
+            )     
+        }
+    }
 
     if(active === false){
         return (
@@ -14,16 +47,15 @@ export function NavMenu(){
         return (
             <div className="navmenu">
                 <div className="navmenu-button" onClick={() => setActive(false)}>Close</div>
-                <ul className="navmenu-list">
-                    <li className="navmenu-item">Log In</li>
-                    <li className="navmenu-item">Sign Up</li>
-                    <li className="navmenu-item">About</li>
-                    <li className="navmenu-item">Your Profile</li>
-                    <li className="navmenu-item">Log Out</li>
-                </ul>
+                    {button_manager()}
             </div>
         )
     }
 }
 
-export default NavMenu;
+const mapStateToProps = state => ({
+    loggedIn: state.auth.currentUser !== null,
+    // loading: state.auth.loading
+});
+
+export default withRouter(connect(mapStateToProps)(NavMenu));
