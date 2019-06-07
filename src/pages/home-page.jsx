d8bca5a781414acc922d8bd53c53788f28991ea4
treyliebscher <wcliebsc@gmail.com>
2019-06-07 09:46:56 -0400

    cleanup

--- src/pages/home-page.jsx
+++ src/pages/home-page.jsx
@@ -1,33 +1,34 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
-import { browsePosts } from '../actions/posts';
+import { browsePosts, viewPostById } from '../actions/posts';
 import QuickViewPost from '../components/posts/quickview-post';
 
 export class HomePage extends React.Component {
 
     componentDidMount() {
         this.props.dispatch(browsePosts(1));
+
     }
 
     render(){
         const posts = this.props.browse.posts.map((post, index) => {
-           return <QuickViewPost id={post._id} key={index} title={post.title} author={post.author} hive={post.hive.title} />
-        })
-        console.log(this.props.browse.posts)
+           return <QuickViewPost id={post.id} key={index} title={post.title} author={post.author} hive={post.hive.title} />
+        });
+
+
         return (
             <div>
                 {posts}
             </div>
         )
     }
-
-
 }
 
 const mapStateToProps = state => {
     return {
-        browse: state.posts
+        browse: state.posts,
+        view: state.post
     };
 };
 
