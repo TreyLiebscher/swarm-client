41adf39330c1f7b63ea952ad59ec6e265f6f42e4
treyliebscher <wcliebsc@gmail.com>
2019-06-13 12:17:06 -0400

    added hive browsing to hp rather than post, will probably have both in the end

--- src/pages/home-page.jsx
+++ src/pages/home-page.jsx
@@ -2,24 +2,36 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { browsePosts, viewPostById } from '../actions/posts';
+import {browseHives} from '../actions/hives';
 import QuickViewPost from '../components/posts/quickview-post';
+import QuickViewHive from '../components/hives/quickview-hive';
 
 export class HomePage extends React.Component {
 
     componentDidMount() {
-        this.props.dispatch(browsePosts(1));
-
+        // this.props.dispatch(browsePosts(1));
+        this.props.dispatch(browseHives(1));
     }
 
     render(){
-        const posts = this.props.browse.posts.map((post, index) => {
-           return <QuickViewPost id={post.id} key={index} title={post.title} author={post.author} hive={post.hive.title} />
-        });
+        // const posts = this.props.browse.posts.map((post, index) => {
+        //    return <QuickViewPost id={post.id} key={index} title={post.title} author={post.author} hive={post.hive.title} />
+        // });
 
+        const hives = this.props.browse.hives.map((hive, index) => {
+            return <QuickViewHive 
+                    id={hive.id} 
+                    key={index} 
+                    title={hive.title} 
+                    mission={hive.mission} 
+                    members={hive.members} 
+                    posts={hive.posts} 
+                    />
+        });
 
         return (
             <div>
-                {posts}
+                {hives}
             </div>
         )
     }
@@ -27,7 +39,7 @@ export class HomePage extends React.Component {
 
 const mapStateToProps = state => {
     return {
-        browse: state.posts,
+        browse: state.hives,
         view: state.post
     };
 };
