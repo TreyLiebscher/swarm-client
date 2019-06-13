0f017a2461214e811fba46b8ce0df2240a57541d
treyliebscher <wcliebsc@gmail.com>
2019-06-13 12:19:20 -0400

    added hive reducers for browse/view

---
+++ src/reducers/hives.js
@@ -0,0 +1,61 @@
+import {
+    GEN_BROWSE_REQUEST,
+    GEN_BROWSE_SUCCESS,
+    GEN_BROWSE_ERROR,
+    VIEW_HIVE_REQUEST,
+    VIEW_HIVE_SUCCESS,
+    VIEW_HIVE_ERROR
+} from '../actions/hives';
+
+const browseState = {
+    hives: [],
+    loading: false,
+    error: null
+};
+
+export function hiveBrowse(state = browseState, action) {
+    if (action.type === GEN_BROWSE_REQUEST) {
+        const changedState = {loading: true, error: null};
+        const newState = {...state, ...changedState};
+        return newState;
+    }
+    else if (action.type === GEN_BROWSE_SUCCESS) {
+        const changedState = {hives: action.hives, loading: false, error: null};
+        const newState = {...state, ...changedState};
+        return newState;
+    }
+    else if (action.type === GEN_BROWSE_ERROR) {
+        const changedState = {loading: false, error: action.error};
+        const newState = {...state, ...changedState};
+        return newState;
+    }
+
+    return state;
+}
+
+const viewState = {
+    hive: {},
+    posts: [],
+    loading: false,
+    error: null
+};
+
+export function hiveView(state = viewState, action) {
+    if (action.type === VIEW_HIVE_REQUEST) {
+        const changedState = {loading: true, error: null};
+        const newState = {...state, ...changedState};
+        return newState;
+    }
+    else if (action.type === VIEW_HIVE_SUCCESS) {
+        const changedState = {hive: action.hive, posts: action.hive.posts, loading: false, error: null};
+        const newState = {...state, ...changedState};
+        return newState;
+    }
+    else if (action.type === VIEW_HIVE_ERROR) {
+        const changedState = {loading: false, error: action.error};
+        const newState = {...state, ...changedState};
+        return newState;
+    }
+
+    return state;
+}
