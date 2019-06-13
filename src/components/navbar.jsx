209f00e8964a4511fe1c2e6989e4a7ed35913cfd
treyliebscher <wcliebsc@gmail.com>
2019-06-13 12:28:51 -0400

    added Link for homepage when logo is clicked

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -9,9 +9,13 @@ export class NavBar extends React.Component {
     render(){
         return (
             <div className="navbar">
-                <h1 className="title">
-                    <span className="s1 logo">S</span><span className="s2 logo">W</span><span className="s3 logo">A</span><span className="s4 logo">R</span><span className="s5 logo">M</span><span className="s6 logo">E</span><span className="s7 logo">R</span>
-                </h1>
+                <div className="logo-box">
+                    <Link to={`/`} className="logo-link">
+                        <h1 className="title">
+                            <span className="s1 logo">S</span><span className="s2 logo">W</span><span className="s3 logo">A</span><span className="s4 logo">R</span><span className="s5 logo">M</span><span className="s6 logo">E</span><span className="s7 logo">R</span>
+                        </h1>
+                    </Link>
+                </div>
                 <NavMenu props={this.props}/>
             </div>
         )
