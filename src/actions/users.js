e418820b410e73f1fb7a0fecbad8c0767fa616b2
treyliebscher <wcliebsc@gmail.com>
2019-06-14 11:41:43 -0400

    fetching profile action

--- src/actions/users.js
+++ src/actions/users.js
@@ -25,3 +25,32 @@ export const registerUser = user => dispatch => {
             }
         });
 };
+
+export const FETCH_PROFILE_SUCCESS = 'FETCH_PROFILE_SUCCESS';
+export const fetchProfileSuccess = (profile) => ({
+    type: FETCH_PROFILE_SUCCESS,
+    profile
+});
+
+export const FETCH_PROFILE_ERROR = 'FETCH_PROFILE_ERROR';
+export const fetchProfileError = error => ({
+    type: FETCH_PROFILE_ERROR,
+    error
+});
+
+export const getProfile = user => (dispatch, getState) => {
+    const AUTH_TOKEN = getState().auth.authToken;
+    
+    return fetch(`${API_BASE_URL}users/profile/home`, {
+        method: 'GET',
+        headers: {
+            'Authorization': `Bearer ${AUTH_TOKEN}`
+        }
+    })
+        .then(res => normalizeResponseErrors(res))
+        .then(res => res.json())
+        .then((profile) => dispatch(fetchProfileSuccess(profile)))
+        .catch(err => {
+            dispatch(fetchProfileError(err));
+        });
+};
