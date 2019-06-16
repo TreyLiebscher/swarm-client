112a7ef6d5e8b7bf4b1c07cd9fd825c6ab5e2325
treyliebscher <wcliebsc@gmail.com>
2019-06-16 15:10:00 -0400

    added a reducer for building hive actions

--- src/reducers/hives.js
+++ src/reducers/hives.js
@@ -4,7 +4,9 @@ import {
     GEN_BROWSE_ERROR,
     VIEW_HIVE_REQUEST,
     VIEW_HIVE_SUCCESS,
-    VIEW_HIVE_ERROR
+    VIEW_HIVE_ERROR,
+    BUILD_HIVE_SUCCESS,
+    BUILD_HIVE_ERROR
 } from '../actions/hives';
 
 const browseState = {
@@ -52,6 +54,35 @@ export function hiveView(state = viewState, action) {
         return newState;
     }
     else if (action.type === VIEW_HIVE_ERROR) {
+        const changedState = {loading: false, error: action.error};
+        const newState = {...state, ...changedState};
+        return newState;
+    }
+
+    return state;
+}
+
+const buildState = {
+    title: '',
+    mission: '',
+    posts: [],
+    loading: false,
+    error: null
+};
+
+export function hiveBuild(state = buildState, action) {
+    if (action.type === BUILD_HIVE_SUCCESS) {
+        const changedState = {
+            title: action.hive.hive.title,
+            mission: action.hive.hive.mission,
+            posts: action.hive.hive.posts,
+            loading: false,
+            error: null
+        }
+        const newState = {...state, ...changedState};
+        return newState;
+    }
+    else if (action.type === BUILD_HIVE_ERROR) {
         const changedState = {loading: false, error: action.error};
         const newState = {...state, ...changedState};
         return newState;
