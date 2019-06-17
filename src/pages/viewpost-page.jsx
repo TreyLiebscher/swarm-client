68c9597f0af3be5769772fcede79b4fe0276828c
treyliebscher <wcliebsc@gmail.com>
2019-06-17 19:29:19 -0400

    small adjustments

--- src/pages/viewpost-page.jsx
+++ src/pages/viewpost-page.jsx
@@ -5,19 +5,16 @@ import {viewPostById} from '../actions/posts';
 export class ViewPostPage extends React.Component {
 
     componentDidMount(){
-        this.props.dispatch(viewPostById(this.props.match.params.id))
+        return this.props.dispatch(viewPostById(this.props.match.params.id))
     }
 
     render(){
         const post = this.props.view.post;
         // TODO Hive is being populated during request and wont render, must solve this
-        console.log(post.hive)
         return (
             <div className="viewpost">
                 <h2>{post.title}</h2>
                 <h3>By: {post.author}</h3>
-                
-                {/* <p>Posted In: {post.hive.title}</p> */}
                 <br />
                 <p>{post.body}</p>
                 <p>Comments: {post.comments}</p>
