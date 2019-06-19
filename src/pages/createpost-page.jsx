c0c0885fdf8564d7378201a983b72b70e35eb437
treyliebscher <wcliebsc@gmail.com>
2019-06-19 09:58:30 -0400

    if user not logged in, they receive link to loginPage

--- src/pages/createpost-page.jsx
+++ src/pages/createpost-page.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import {connect} from 'react-redux';
-import {withRouter, Redirect} from 'react-router-dom';
+import {withRouter, Redirect, Link} from 'react-router-dom';
 import {getProfile} from '../actions/users';
 import slugify from 'slugify';
 
@@ -12,15 +12,28 @@ export class CreatePostPage extends React.Component {
     }
 
     render(){
-        return (
-            <div>
-                <CreatePostForm hive={this.props.match.params.id} currentPost={this.props.post.post.post}/>
-            </div>
-        )
+
+        if(this.props.loggedIn === false){
+            return (
+                <div>
+                    <p>Login to create posts</p>
+                    <Link to="/login">Login</Link>
+                </div>
+            )
+        }
+
+        else {
+            return (
+                <div>
+                    <CreatePostForm hive={this.props.match.params.id} currentPost={this.props.post.post.post}/>
+                </div>
+            )
+        }
     }
 }
 
 const mapStateToProps = state => ({
+    loggedIn: state.auth.currentUser !== null,
     user: state.userProfile,
     post: state.createPost
 });
