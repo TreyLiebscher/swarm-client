6bcee6a4eeeb412587f73b6932352fbae215c600
treyliebscher <wcliebsc@gmail.com>
2019-06-19 12:07:35 -0400

    added rendered comments + comment form to post view page

--- src/pages/viewpost-page.jsx
+++ src/pages/viewpost-page.jsx
@@ -1,23 +1,36 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import {viewPostById} from '../actions/posts';
+import CreateCommentForm from '../components/forms/createComment-form';
+import { getProfile } from '../actions/users';
 
 export class ViewPostPage extends React.Component {
+    constructor(props, context) {
+        super(props, context);
+    }
 
     componentDidMount(){
-        return this.props.dispatch(viewPostById(this.props.match.params.id))
+        this.props.dispatch(viewPostById(this.props.match.params.id))
+        .then(res => this.props.dispatch(getProfile()))
     }
 
     render(){
         const post = this.props.view.post;
-        // TODO Hive is being populated during request and wont render, must solve this
+        const comments = this.props.comments.map((comment, index) => {
+            return <li key={index}>
+                    <p>{comment.author} says:</p>
+                    <p>{comment.body}</p>
+                    </li>
+        });
+
         return (
             <div className="viewpost">
                 <h2>{post.title}</h2>
                 <h3>By: {post.author}</h3>
                 <br />
                 <p>{post.body}</p>
-                <p>Comments: {post.comments}</p>
+                <ul>{comments}</ul>
+                <CreateCommentForm post={post}/>
             </div>
         )
     }
@@ -25,7 +38,9 @@ export class ViewPostPage extends React.Component {
 
 const mapStateToProps = state => {
     return {
-        view: state.post
+        view: state.post,
+        comments: state.post.comments,
+        user: state.userProfile
     };
 };
 
