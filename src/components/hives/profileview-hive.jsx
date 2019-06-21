85003cb64c26001e05fe105b95321c87d21ca375
treyliebscher <wcliebsc@gmail.com>
2019-06-21 10:06:21 -0400

    component for viewing a users hives on profile page

---
+++ src/components/hives/profileview-hive.jsx
@@ -0,0 +1,54 @@
+import React from 'react';
+import {Link, Redirect, withRouter} from 'react-router-dom';
+import QuickViewHive from './quickview-hive';
+import slugify from 'slugify';
+import './profileview-hive.css';
+
+export class ProfileViewHive extends React.Component {
+    constructor(props) {
+        super(props);
+        this.displayHives = this.displayHives.bind(this);
+        this.state = {
+            visible: false
+        }
+    }
+
+    displayHives(){
+        if(this.state.visible === false){
+            this.setState({visible: true});
+        } else {
+            this.setState({visible: false});
+        }
+    }
+
+    render(){
+
+        const hives = this.props.hives.map((hive, index) => {
+            return <QuickViewHive 
+            id={hive.id} 
+            key={index} 
+            title={hive.title} 
+            mission={hive.mission} 
+            members={hive.members.length} 
+            posts={hive.posts.length} 
+            />
+        })
+
+        if(this.state.visible === false){
+            return (
+                <div className="profile-view-hive-container" onClick={this.displayHives}>
+                    <p className="profile-view-hive-label">HIVES</p>
+                </div>
+            )
+        } else {
+            return (
+                <div className="profile-view-hive-container" onClick={this.displayHives}>
+                    <p className="profile-view-hive-label visible">HIVES</p>
+                    {hives}
+                </div>
+            )
+        }
+    }
+}
+
+export default ProfileViewHive;
