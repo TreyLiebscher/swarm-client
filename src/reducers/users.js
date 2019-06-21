ad1c3c06b8ce660cf149a9e87790e62ee7dfe685
treyliebscher <wcliebsc@gmail.com>
2019-06-21 08:00:14 -0400

    reduced nesting, better flow

--- src/reducers/users.js
+++ src/reducers/users.js
@@ -4,14 +4,31 @@ import {
 } from '../actions/users';
 
 const userState = {
-    user: {},
+    // user: {},
+    username: '',
+    email: '',
+    id: '',
+    comments: [],
+    posts: [],
+    hives: [],
     loading: false,
     error: null
 };
 
 export function profileReducer(state = userState, action) {
     if (action.type === FETCH_PROFILE_SUCCESS) {
-        const changedState = {user: action.profile, loading: false, error: null};
+        console.log('kiwi', action.profile.profile)
+        const changedState = {
+            // user: action.profile,
+            username: action.profile.profile.username,
+            email: action.profile.profile.email,
+            id: action.profile.profile.id,
+            comments: action.profile.profile.comments,
+            posts: action.profile.profile.posts,
+            hives: action.profile.profile.hives, 
+            loading: false, 
+            error: null
+        };
         const newState = {...state, ...changedState};
         return newState;
     }
