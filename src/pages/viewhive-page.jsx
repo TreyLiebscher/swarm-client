b7b79ae096f5d069538792c4744762d82178ab95
treyliebscher <wcliebsc@gmail.com>
2019-06-22 09:38:49 -0400

    added join button for joining a hive when viewing it

--- src/pages/viewhive-page.jsx
+++ src/pages/viewhive-page.jsx
@@ -1,26 +1,39 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import {Link} from 'react-router-dom';
-import {viewHiveByTitle} from '../actions/hives';
+import {viewHiveByTitle, joinHive} from '../actions/hives';
 import QuickViewPost from '../components/posts/quickview-post';
 import slugify from 'slugify';
 
 export class ViewHivePage extends React.Component {
+    constructor(props){
+        super(props);
+        this.joinHive = this.joinHive.bind(this)
+    }
+
 
     componentDidMount(){
         this.props.dispatch(viewHiveByTitle(this.props.match.params.title))
     }
 
+    joinHive(){
+        return this.props.dispatch(joinHive(this.props.view))
+    }
+
+
     render(){
-        const hive = this.props.view.hive;
+        const hive = this.props.view;
         const posts = this.props.view.posts.map((post, index) => {
            return <QuickViewPost id={post._id} key={index} title={post.title} author={post.author} hive={post.hive.title} comments={post.comments.length} tags={post.tags} />
         });
+        const members = hive.members.length;
 
         return (
             <div className="viewHive">
                 <h2>{hive.title}</h2>
                 <h3>{hive.mission}</h3>
+                <p>Members: {members}</p>
+                <button onClick={this.joinHive}>Join</button>
                 <Link to={`/posts/create/${hive.id}`}>Create Post</Link>
                 {posts}
             </div>
@@ -30,7 +43,8 @@ export class ViewHivePage extends React.Component {
 
 const mapStateToProps = state => {
     return {
-        view: state.hive
+        view: state.hive,
+        user: state.userProfile
     };
 };
 
