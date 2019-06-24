6e2e35f83d275a8d577d19126b9ef6cd63c88631
treyliebscher <wcliebsc@gmail.com>
2019-06-24 10:24:54 -0400

    pagination now functional, inf scroll planned for later

--- src/pages/home-page.jsx
+++ src/pages/home-page.jsx
@@ -2,16 +2,42 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { browsePosts, viewPostById } from '../actions/posts';
+import LoadingMessage from '../components/loading-message';
 import {browseHives} from '../actions/hives';
 import QuickViewPost from '../components/posts/quickview-post';
 import QuickViewHive from '../components/hives/quickview-hive';
 import './home-page.css';
 
 export class HomePage extends React.Component {
+    constructor(props){
+        super(props);
+        this.nextPage = this.nextPage.bind(this);
+        this.prevPage = this.prevPage.bind(this);
+        this.state = {
+            page: 1,
+            moreItems: true
+        }
+    }
+
 
     componentDidMount() {
         // this.props.dispatch(browsePosts(1));
-        this.props.dispatch(browseHives(1));
+        this.props.dispatch(browseHives(this.state.page));
+    }
+
+    nextPage(){
+        if((this.props.browse.totalHives / 10) > this.state.page){
+            this.setState({page: this.state.page + 1});
+            return this.props.dispatch(browseHives(this.state.page + 1))
+        }
+
+    }
+
+    prevPage(){
+        if(this.state.page > 1){
+            this.setState({page: this.state.page - 1})
+            return this.props.dispatch(browseHives(this.state.page - 1))
+        }
     }
 
     render(){
@@ -30,9 +56,28 @@ export class HomePage extends React.Component {
                     />
         });
 
+        const backButton = () => {
+            if(this.props.browse.currentPage > 1){
+                return <button onClick={this.prevPage}>Back</button> 
+            }
+        }
+
+        const nextButton = () => {
+            if(this.props.browse.currentPage < this.props.browse.pages){
+                return <button onClick={this.nextPage}>Next</button>
+            }
+        }
+
         return (
             <div className="home-page-container">
+                <LoadingMessage loading={this.props.browse.loading} />
+                <p>Page {this.props.browse.currentPage}/{this.props.browse.pages}</p>
+                {backButton()}
+                {nextButton()}
                 {hives}
+                <p>Page {this.props.browse.currentPage}/{this.props.browse.pages}</p>
+                {backButton()}
+                {nextButton()}
             </div>
         )
     }
