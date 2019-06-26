bf7997e4aacf2940418da921d7c80503ad77a920
treyliebscher <wcliebsc@gmail.com>
2019-06-26 17:10:31 -0400

    fixed bug with non-rated posts

--- src/components/posts/quickview-post.jsx
+++ src/components/posts/quickview-post.jsx
@@ -16,7 +16,11 @@ export class QuickViewPost extends React.Component {
             }
             return urlTitle;
         }
-        const ratings = Math.round(this.props.ratings.reduce((a, b) => a + b) / this.props.ratings.length);
+        const ratings = () => {
+            if(this.props.ratings.length !== 0){
+                return Math.round(this.props.ratings.reduce((a, b) => a + b) / this.props.ratings.length)
+            }
+        }
         let tags = this.props.tags.map((tag, index) => <li key={index} className="quickview-post-tag">{tag}</li>);
         const bodyPreview = this.props.body.substring(0, 50);
         return (
@@ -24,7 +28,7 @@ export class QuickViewPost extends React.Component {
                 <Link className="quickview-post-title-link" to={`/posts/view/${this.props.id}/${urlTitleShorten(urlTitle)}`}><h3 className="quickview-post-title">{this.props.title}</h3></Link>
                 <p>{this.props.hive}</p>
                 <p>{this.props.author}</p>
-                <Ratings ratings={ratings} length={this.props.ratings.length}/>
+                <Ratings ratings={ratings()} length={this.props.ratings.length}/>
                 <p className="quickview-post-bodypreview"><i>{bodyPreview}...</i></p>
                 <ul className="quickview-post-tags-container">{tags}</ul>
                 <p>Comments: {this.props.comments}</p>
