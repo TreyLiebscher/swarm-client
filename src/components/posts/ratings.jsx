5a6946cd364f44f407464815e7ef34b7d18cfd11
treyliebscher <wcliebsc@gmail.com>
2019-06-27 12:18:40 -0400

    added unicode symbols for ratings display

--- src/components/posts/ratings.jsx
+++ src/components/posts/ratings.jsx
@@ -7,19 +7,19 @@ export default class Ratings extends React.Component {
         const ratings = this.props.ratings;
         const displayRating = () => {
             if(ratings === 5){
-                return <p className="quickview-post-rating five">O O O O O <span className="rating-count">({this.props.length})</span></p>
+                return <p className="quickview-post-rating five"> &#x2605; &#x2605; &#x2605; &#x2605; &#x2605; <span className="rating-count">({this.props.length} reviews)</span></p>
             }
             else if(ratings === 4){
-                return <p className="quickview-post-rating four">O O O O <span className="rating-count">({this.props.length})</span></p>
+                return <p className="quickview-post-rating four">&#x2605; &#x2605; &#x2605; &#x2605; &#x2606; <span className="rating-count">({this.props.length} reviews)</span></p>
             }
             else if(ratings === 3){
-                return <p className="quickview-post-rating three">O O O <span className="rating-count">({this.props.length})</span></p>
+                return <p className="quickview-post-rating three">&#x2605; &#x2605; &#x2605; &#x2606; &#x2606; <span className="rating-count">({this.props.length} reviews)</span></p>
             }
             else if(ratings === 2){
-                return <p className="quickview-post-rating two">O O <span className="rating-count">({this.props.length})</span></p>
+                return <p className="quickview-post-rating two">&#x2605; &#x2605; &#x2606; &#x2606; &#x2606; <span className="rating-count">({this.props.length} reviews)</span></p>
             }
             else if(ratings === 1){
-                return <p className="quickview-post-rating one">O <span className="rating-count">({this.props.length})</span></p>
+                return <p className="quickview-post-rating one">&#x2605; &#x2606; &#x2606; &#x2606; &#x2606; <span className="rating-count">({this.props.length} reviews)</span></p>
             }
         }
 
