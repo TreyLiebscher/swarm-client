e51f4b7a3dd43465997ed8f82f5f26e8afa36116
treyliebscher <wcliebsc@gmail.com>
2019-09-24 16:36:49 -0400

    added clear notifications functionality

--- src/reducers/users.js
+++ src/reducers/users.js
@@ -1,6 +1,8 @@
 import {
     FETCH_PROFILE_SUCCESS,
-    FETCH_PROFILE_ERROR    
+    FETCH_PROFILE_ERROR,
+    CLEAR_NOTIFICATION_SUCCESS,
+    CLEAR_NOTIFICATION_ERROR    
 } from '../actions/users';
 
 const userState = {
@@ -17,7 +19,6 @@ const userState = {
 };
 
 export function profileReducer(state = userState, action) {
-    console.log('kiwi', action.profile)
     if (action.type === FETCH_PROFILE_SUCCESS) {
         const changedState = {
             username: action.profile.profile.username,
@@ -35,6 +36,27 @@ export function profileReducer(state = userState, action) {
         return newState;
     }
     else if (action.type === FETCH_PROFILE_ERROR) {
+        const changedState = {loading: false, error: action.error};
+        const newState = {...state, ...changedState};
+        return newState;
+    }
+    else if (action.type === CLEAR_NOTIFICATION_SUCCESS) {
+        const changedState = {
+            username: action.profile.profile.username,
+            email: action.profile.profile.email,
+            id: action.profile.profile.id,
+            comments: action.profile.profile.comments,
+            posts: action.profile.profile.posts,
+            ratedPosts: action.profile.profile.ratedPosts,
+            hives: action.profile.profile.hives,
+            notifications: action.profile.profile.notifications, 
+            loading: false, 
+            error: null
+        };
+        const newState = {...state, ...changedState};
+        return newState;
+    }
+    else if (action.type === CLEAR_NOTIFICATION_ERROR) {
         const changedState = {loading: false, error: action.error};
         const newState = {...state, ...changedState};
         return newState;
