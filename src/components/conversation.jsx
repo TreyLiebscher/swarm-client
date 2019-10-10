11e4094b72c9e6c50c143529c648728f8e8fd2aa
treyliebscher <wcliebsc@gmail.com>
2019-10-10 13:02:35 -0400

    better formatting for message display

--- src/components/conversation.jsx
+++ src/components/conversation.jsx
@@ -4,42 +4,67 @@ import {connect} from 'react-redux';
 import './conversation.css'
 
 export default class Conversation extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            visible: false
+        }
+        this.displayMessages = this.displayMessages.bind(this);
+    }
 
+    displayMessages(){
+        if(this.state.visible === false){
+            this.setState({visible: true})
+        }
+        else {
+            this.setState({visible: false})            
+        }
+    }
 
 
 
     render(){
         const messages = this.props.messages.map((message, index) => {
             if(message.user === this.props.user){
-                return <p className="user-message">{message.body}</p>
+                return <p className="user-message" key={index}>{message.body}</p>
             } else {
-                return <p className="receiver-message">{message.body}</p>
+                return <p className="receiver-message" key={index}>{message.body}</p>
             }
 
         });
         let receiver;
-        const users = this.props.users.map((user) => {
+        const users = this.props.users.map((user, index) => {
             if(user._id === this.props.user){
-                return <p>You</p>
+                return;
             } else {
                 receiver = user._id
-                return <p>{user.username}</p>
+                return <p key={index}>{user.username}</p>
             }
         })
 
-        return (
-            <div className="conversation-container">
-                <div className="conversation-people">
-                    <p>Conversation with</p>
-                    {users}
+        if(this.state.visible === true){
+            return (
+                <div className="conversation-container">
+                    <div className="conversation-people" onClick={this.displayMessages}>
+                        {users}
+                    </div>
+                    {messages}
+                    <SendMessageForm 
+                        sender={this.props.user}
+                        receiver={receiver}
+                        conversation={this.props.convoId}
+                    />
                 </div>
-                {messages}
-                <SendMessageForm 
-                    sender={this.props.user}
-                    receiver={receiver}
-                    conversation={this.props.convoId}
-                />
-            </div>
-        )
+            )            
+        }
+        else {
+            return (
+                <div className="conversation-container">
+                    <div className="conversation-people" onClick={this.displayMessages}>
+                        {users}
+                    </div>
+                </div>
+            )
+        }
     }
 }
