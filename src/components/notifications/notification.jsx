299d2ddcb0c163bc7a60ac08a82434842a18e591
treyliebscher <wcliebsc@gmail.com>
2019-10-17 15:29:45 -0400

    comment reply notifications now link to the individual comment on the post

--- src/components/notifications/notification.jsx
+++ src/components/notifications/notification.jsx
@@ -19,39 +19,40 @@ export class Notification extends React.Component {
 
     render(){
         if(this.props.notification.type === 'NewComment'){
-            let urlTitle = slugify(this.props.notification.postTitle);
-            const urlTitleShorten = (urlTitle) => {
-                if(urlTitle.length >= 25){
-                    urlTitle = urlTitle.slice(0, 25);
-                }
-                return urlTitle;
-            }
+            // let urlTitle = slugify(this.props.notification.postTitle);
+            // const urlTitleShorten = (urlTitle) => {
+            //     if(urlTitle.length >= 25){
+            //         urlTitle = urlTitle.slice(0, 25);
+            //     }
+            //     return urlTitle;
+            // }
             return (
                 <div className="notification-container">
                     <button className="notification-clear-button" onClick={this.handleClearNotification}>
                         <div className="notification-info">
-                            <Link to={`/posts/view/${this.props.notification.post}/${urlTitleShorten(urlTitle)}`} className="notification-link">{this.props.notification.message} <span className="notification-post-title">{this.props.notification.postTitle}</span></Link>
+                            <Link to={`/posts/comments/${this.props.notification.comment._id}`} className="notification-link">{this.props.notification.message} <span className="notification-post-title">{this.props.notification.postTitle}</span></Link>
                         </div>
                     </button>               
                     <div className="notification-info content">
                         <p>{this.props.notification.comment.body}</p>
                     </div>
+                    <button onClick={this.handleClearNotification}>clear</button>
                 </div>
             )
         }
         else if(this.props.notification.type === 'NewReply'){
-            let urlTitle = slugify(this.props.notification.postTitle);
-            const urlTitleShorten = (urlTitle) => {
-                if(urlTitle.length >= 25){
-                    urlTitle = urlTitle.slice(0, 25);
-                }
-                return urlTitle;
-            }
+            // let urlTitle = slugify(this.props.notification.postTitle);
+            // const urlTitleShorten = (urlTitle) => {
+            //     if(urlTitle.length >= 25){
+            //         urlTitle = urlTitle.slice(0, 25);
+            //     }
+            //     return urlTitle;
+            // }
             return (
                 <div className="notification-container">
                     <button className="notification-clear-button" onClick={this.handleClearNotification}>
                         <div className="notification-info">
-                            <Link to={`/posts/view/${this.props.notification.post}/${urlTitleShorten(urlTitle)}`} className="notification-link">{this.props.notification.message} <span className="notification-post-title">{this.props.notification.postTitle}</span></Link>               
+                            <Link to={`/posts/comments/${this.props.notification.comment.post}`} className="notification-link">{this.props.notification.message} <span className="notification-post-title">{this.props.notification.postTitle}</span></Link>               
                         </div>
                     </button>
                     <div className="notification-info content">
