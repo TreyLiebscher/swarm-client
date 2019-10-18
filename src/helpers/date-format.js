b1eca2436d6c07af8dfc5e5feb95bf4efb83dd9e
treyliebscher <wcliebsc@gmail.com>
2019-10-18 12:31:54 -0400

    displays time since creation rather than creation date

--- src/helpers/date-format.js
+++ src/helpers/date-format.js
@@ -1,7 +1,22 @@
 export default function FormatDate(isoStr){
     const timeStamp = new Date(isoStr);
-    const date = timeStamp.toLocaleString().replace(/\//g, '.');
-    return date;
+    const currentTime = new Date();
+    const diff = currentTime.getTime() - timeStamp.getTime()
+       
+    const diffYears = Math.round(diff / (1000 * 3600 * 24 * 365));
+    const diffDays = Math.round(diff / (1000 * 3600 * 24));
+    const diffHours = Math.round(diff / (1000 * 3600));
+    const diffMinutes = Math.round(diff / (1000 * 60));
+
+    if(diffMinutes < 60){
+        return `${diffMinutes}m`;
+    } else if(diffHours < 24){
+        return `${diffHours}h`;
+    } else if(diffDays < 365){
+        return `${diffDays}d`;
+    } else {
+        return `${diffYears}y`;
+    }
 };
 
 export function FormatYear(isoStr){
